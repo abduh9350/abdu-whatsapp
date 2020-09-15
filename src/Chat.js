2edac0da545aff5776d345194b02590076b03637
Abdu Jemal <abduh9350@gmail.com>
2020-09-14 21:01:42 -0400

    frontend about to finish

--- src/Chat.js
+++ src/Chat.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Chat.css";
-import { Avatar } from "@material-ui/core";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import MicIcon from "@material-ui/icons/Mic";
+import { Avatar, IconButton } from "@material-ui/core";
 function Chat() {
   return (
     <div className="chat">
@@ -10,6 +15,47 @@ function Chat() {
           <h3>Room Name</h3>
           <p>Last seen minutrs ago</p>
         </div>
+
+        <div className="chat_headerRight">
+          <IconButton>
+            <SearchOutlinedIcon />
+          </IconButton>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="chat_body">
+        <p className="chat_message">
+          <span className="chat_name">Abdu</span>
+          This is message 1
+          <span className="chat_time">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className="chat_message chat_reciever">
+          <span className="chat_name">Kedir</span>
+          This is message 1
+          <span className="chat_time">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className="chat_message">
+          <span className="chat_name">Abdu</span>
+          This is message 1
+          <span className="chat_time">{new Date().toUTCString()}</span>
+        </p>
+      </div>
+
+      <div className="chat_footer">
+        <InsertEmoticonIcon />
+        <form>
+          <input placeholder="Type a message" type="text" />
+          <button type="submit">Send message</button>
+        </form>
+        <MicIcon />
       </div>
     </div>
   );
